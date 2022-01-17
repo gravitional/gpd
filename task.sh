2e575c8d3f5084b4ab8c3b37ab0fd68d8f828884
gravitional <albertains@163.com>
2022-01-18 00:34:26 +0800

    fix task

--- task.sh
+++ task.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-#declare -a lbdLst=("0.80" "0.90" "1.00")
-#
-#for lbd in ${lbdLst[@]}; do
-#    wolframscript -script ff.numeric-eval.wl --Lbd-num $lbd --Lbd-fit $lbd &>~/test/full-num-$Lbd-fit-$lbd.log
-#done
+declare -a lbdLst=("0.75" "0.80" "0.90" "1.00")
+
+for lbd in ${lbdLst[@]}; do
+    wolframscript -script ff.numeric-eval.wl --Lbd-num $lbd --Lbd-fit $lbd &>~/test/full-num-$lbd-fit-$lbd.log
+done
 
 declare -a lbdLst=("0.80" "1.00")
 
 for lbd in ${lbdLst[@]}; do
-    wolframscript -script ff.numeric-eval.wl --Lbd-num $lbd --Lbd-fit '0.90' &>~/test/full-num-$Lbd-fit-'0.90'.log
+    wolframscript -script ff.numeric-eval.wl --Lbd-num $lbd --Lbd-fit '0.90' &>~/test/full-num-$lbd-fit-'0.90'.log
 done
