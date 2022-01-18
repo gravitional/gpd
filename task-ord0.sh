f4edbf45cd2a8b717847a99d0417be10f6021126
gravitional <albertains@163.com>
2022-01-18 20:05:46 +0800

    minor

---
+++ task-ord0.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+declare -a lbdLst=("0.75" "0.80" "0.90" "1.00")
+
+for lbd in ${lbdLst[@]}; do
+    wolframscript -script ff.numeric-eval.wl --ord '\$ord0' --Lbd-num $lbd --Lbd-fit $lbd 
+done
+
+declare -a lbdLst=("0.80" "1.00")
+
+for lbd in ${lbdLst[@]}; do
+    wolframscript -script ff.numeric-eval.wl --ord '\$ord0' --Lbd-num $lbd --Lbd-fit '0.90' 
+done
