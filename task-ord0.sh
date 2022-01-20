c4eb1d78522f672d92f400399dbbbb650319ad1d
gravitional <albertains@163.com>
2022-01-21 00:02:03 +0800

    minor

--- task-ord0.sh
+++ task-ord0.sh
@@ -6,8 +6,8 @@ for lbd in ${lbdLst[@]}; do
     wolframscript -script ff.numeric-eval.wl --ord '\$ord0' --Lbd-num $lbd --Lbd-fit $lbd 
 done
 
-declare -a lbdLst=("0.80" "1.00")
-
-for lbd in ${lbdLst[@]}; do
-    wolframscript -script ff.numeric-eval.wl --ord '\$ord0' --Lbd-num $lbd --Lbd-fit '0.90' 
-done
+#declare -a lbdLst=("0.80" "1.00")
+#
+#for lbd in ${lbdLst[@]}; do
+#    wolframscript -script ff.numeric-eval.wl --ord '\$ord0' --Lbd-num $lbd --Lbd-fit '0.90' 
+#done
